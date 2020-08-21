9dd54a818c0f8385c59b60ab6fd822a4954c1761
verosoft.roberval <roberval@verosof.com.br>
2020-08-21 10:21:19 -0300

    first commit

---
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -0,0 +1,26 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class DropColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.drop(labels=self.columns, axis='columns')
+        
+class LimiteValorNota (BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        for i in self.columns:
+           X.loc[X [i] > 10, i] = 10
+        return X
